5379d6213bfc0cc6cfcd47fe60300aca55859dc9
orjon <github@orjon.com>
2019-02-19 19:30:55 +0000

    JQuery div grid generation working

--- scripts/mainGRID.js
+++ scripts/mainGRID.js
@@ -19,6 +19,21 @@ $(() => {
 
 
 
+  function createBoard() {
+    let gridNumbers = '' //40 //starting point for visible grid
+    for (let i=0; i<20; i++){ //Create 20 rows of..
+
+      $('<div></div>').addClass('row').appendTo('#gameGrid') //Divs with .row, with
+    }
+    for (let j=0; j<10; j++) {
+      $('.row').append('<div>'+gridNumbers+'</div>') //10 Divs with gridNumbers++
+      // gridNumbers++
+    }
+    $('#gameGrid .row').children().addClass('square') //
+  }
+
+createBoard()
+
 
   function clockTick() {
     // if (anyFalling() === false) {
